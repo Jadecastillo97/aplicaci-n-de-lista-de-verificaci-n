8f64e5256ecfca357c2ceacac6850ef7ecbe3cab
daylerjeff199906 <daylersan@gmail.com>
2024-12-07 23:14:00 -0500

    feat: agregar diseño de contenido al panel de administración

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,6 +1,13 @@
+import { ContentLayout } from "@/components/admin-panel/content-layout"
 import { AdminDashboard } from "@/modules/admin"
 import React from "react"
 
 export default function page() {
-  return <AdminDashboard />
+  return (
+    <>
+      <ContentLayout title="Inicio">
+        <AdminDashboard />
+      </ContentLayout>
+    </>
+  )
 }
